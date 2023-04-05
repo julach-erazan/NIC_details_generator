454e47979e195bfdca2f29be226441f4e3b8be25
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2023-04-05 23:04:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PartOne from './components/PartOne';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<h1>Hello</h1>*/}
+      <PartOne/>
+    </div>
+  );
+}
+
+export default App;
